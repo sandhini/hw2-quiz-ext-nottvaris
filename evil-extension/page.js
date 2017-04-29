01fbf89e28195555324d588d80bde4ce0ac09982
nottvaris <nottvaris@gmail.com>
2017-04-29 15:17:42 -0700

    Add files via upload

--- evil-extension/page.js
+++ evil-extension/page.js
@@ -10,87 +10,50 @@ const MATCH_LIST = {
   'THEY\'RE': 'THERE'
 };
 
-//get the body element of the page
-// let body = document.querySelector('body');
-// let space = ' ';
-let space = /\s+/;
-let newline = /\n+/;
-// let newline = '\n';
+let space = /\s+/; // separator 1 for a split function , spaces.
+let newline = /\n+/; //separator 2 for a split function , newlines.
 let arrayOfStrings = null;
 
-function splitString(stringToSplit, separator) {
-  arrayOfStrings = stringToSplit.split(separator);
+function splitString(stringToSplit, separator) { //put a string into an array so that we can loop over and check for matches
+  arrayOfStrings = stringToSplit.split(separator); //use 2 separators to parse out the string (1. spaces 2. newlines)
 } 
 
 
-function transformTextNodes(node) {
+function transformTextNodes(node) { // walk the DOM tree until all text node is found
 
+      if (node.nodeType === Node.TEXT_NODE && node.nodeName !== '<script>' && node.nodeName !== '<style>') { //check if a node is a text node or not
 
-      //get all the text nodes
-      if (node.nodeType === Node.TEXT_NODE && node.nodeName !== '<script>' && node.nodeName !== '<style>') {
-        // console.log(bodyNodes);
-        // console.log(bodyNodes.textContent);
-        // console.log(bodyNodes.childNodes);
+        let text = node.textContent; //get the text content of that text node and store it
+        text.trim(); //trim any whitespaces
 
-        let text = node.textContent;
-        console.log(text);
-        text.trim();
-
-
-
-        //put textContent of textNode into arrays of strings
-        splitString(text, space, newline);
-        console.log(arrayOfStrings);
+        splitString(text, space, newline); //put textContent of the textNode into arrays of strings
+    
 
         for (let i = 0; i < arrayOfStrings.length; i++) {
 
-          if (MATCH_LIST[arrayOfStrings[i]]){
-            arrayOfStrings[i] = MATCH_LIST[arrayOfStrings[i]];
-            //concert a sting array back to string
-            //textContent that
-            let newText = arrayOfStrings.join(" ");
-            newText.trim();
-            node.textContent = newText;
-            // console.log(arrayOfStrings[i]);
-            // console.log(arrayOfStrings);
+          if (MATCH_LIST[arrayOfStrings[i]]){  //if a string in an array matches one of the keys then..
+            arrayOfStrings[i] = MATCH_LIST[arrayOfStrings[i]];  //then replace it witht he key value
+         
+            let newText = arrayOfStrings.join(" "); //convert the array back to a string
+            newText.trim(); //trim any whitespaces
+            node.textContent = newText; //replace the current text content with the new text content
           }
           
-        
-
-
-
-
-
-
-
 
         }
 
-      } else {
+      } else { // if a text node is not found then go into its childnode element to exmaine further (start of the recursion
 
-        //get all childnodes within the document.body except for text content of <script> and <style>
-         for (const child of node.childNodes){
+         for (const child of node.childNodes){ ////go into childnodes of the node element until text node is found (recursion()
           transformTextNodes(child);
       }
 
 }
 
-  
-
-
-
-        //while getting that check if any of them == to the 3 cases we got (1. there, 2. their, 3. they're)
-        //if equal to one of these modify it accordingly
 
   }
 
-
-
- 
-
-  //display the results
-
-transformTextNodes(document.body);
+transformTextNodes(document.body); //pass in the body of the HTML to be walked through its DOM
 
 
 // Log statement to test that the extension loaded properly.
